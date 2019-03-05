d72ad34900658b29fdb785ddfdc100168cf0942e
Araliatrix <agentisaacson@gmail.com>
2019-03-04 19:28:54 -0700

    figured out how to read content page

--- background.js
+++ background.js
@@ -1,41 +1,5 @@
-// const submitButton = document.querySelector("iframe").contentDocument.body;
-// const body = document.querySelector("body");
+const submitButton = document.querySelector(".exportOverlay"); //.quantumWizButtonPaperbuttonLabel
 
-// console.log(submitButton);
-// submitButton.addEventListener("click", function() {
-//   alert("clicked");
-// });
-// body.addEventListener("click", function() {
-//   alert("clicked");
-// });
-
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.webNavigation.addListener(
-    function() {
-      const form1 = document.querySelector("body");
-      let window1 = 0;
-      chrome.runtime.getBackgroundPage(function(Window) {
-        window1 = Window.innerHeight;
-        alert(window1);
-      });
-      return form1;
-    },
-    {
-      url: [
-        {
-          urlMatches:
-            "https://docs.google.com/forms/d/e/1FAIpQLScFVg5sYWwkVbCZHx1nCY6hKhkgMSpU2h9KjV_HdGHY-0OGVQ/viewform"
-        }
-      ]
-    }
-  );
+submitButton.addEventListener("click", () => {
+  console.log("SupYo");
 });
-
-// const submitButton = document.querySelector("body"); //.quantumWizButtonPaperbuttonLabel
-
-// console.log(submitButton);
-// if (submitButton !== null) {
-//   submitButton.addEventListener("click", function() {
-//     alert("clicked");
-//   });
-// }
