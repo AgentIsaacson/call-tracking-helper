73cd614ed046047d72230909c6654e18f3068e36
Araliatrix <agentisaacson@gmail.com>
2019-03-04 22:26:25 -0700

    working on sending data to extension

--- background.js
+++ background.js
@@ -1,5 +1,34 @@
-const submitButton = document.querySelector(".exportOverlay"); //.quantumWizButtonPaperbuttonLabel
+const submitButton = document.querySelector(".exportOverlay");
+const submitButton2 = document.querySelector(
+  ".quantumWizButtonPaperbuttonLabel"
+);
+const options = [...document.querySelectorAll(".exportOption")];
+const radios = [...document.querySelectorAll(".exportLabelWrapper")];
+let data = [];
 
-submitButton.addEventListener("click", () => {
-  console.log("SupYo");
-});
+function getFormData() {
+  data = [];
+  options.forEach((option) => {
+    if (option.classList.contains("isSelected")) {
+      let content = option.querySelector("content").innerHTML;
+      data.push(content);
+    }
+  });
+  radios.forEach((radio) => {
+    let selectedRadio = radio.querySelector(".isChecked");
+    if (selectedRadio != null) {
+      let content = selectedRadio.dataset.value;
+      data.push(content);
+    }
+  });
+  // chrome.runtime.sendMessage({ data: [...data], greeting: "hello" });
+  console.log(chrome.commands)
+  var port = chrome.runtime.connect({ name: "dataTransfer" });
+  port.postMessage({ data: data });
+  port.onMessage.addListener(function(msg) {
+    console.log(msg);
+  });
+}
+
+submitButton.addEventListener("click", () => getFormData());
+submitButton2.addEventListener("click", () => getFormData());
---
+++ script.js
@@ -0,0 +1,13 @@
+const text = document.querySelector("button").innerHTML;
+
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   console.log(sender.tab);
+//   console.log(request);
+// });
+
+chrome.runtime.onConnect.addListener(function(port) {
+  console.assert(port.name == "dataTransfer");
+  port.onMessage.addListener(function(msg) {
+      port.postMessage({ data2: msg });
+  });
+});
