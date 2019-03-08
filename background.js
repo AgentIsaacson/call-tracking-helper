94c4fbee14c2515862b1b6a2828b478bcc71ff34
AgentIsaacson <agentisaacson@gmail.com>
2019-03-08 13:03:53 -0700

    fixed bug so now all submissions are stored, not overridden

--- background.js
+++ background.js
@@ -1,49 +1,45 @@
 const submitButton = document.querySelector(".exportOverlay");
-const submitButton2 = document.querySelector(
-  ".quantumWizButtonPaperbuttonLabel"
-);
+// const submitButton2 = document.querySelector(
+//   ".quantumWizButtonPaperbuttonLabel"
+// );
 const submitButton3 = document.querySelector(
   ".quantumWizButtonPaperbuttonContent"
 );
 const options = [...document.querySelectorAll(".exportOption")];
 const radios = [...document.querySelectorAll(".exportLabelWrapper")];
-
+let data = [];
 function getFormData() {
   chrome.storage.local.get(["responses"], function(result) {
-    console.log("HERE: " + result.responses);
-    if (result.responses) {
+    if (result.responses != undefined) {
       data = result.responses;
-    } else {
-      data = [];
     }
-  });
-  console.log(data);
-  let temp = [];
-  options.forEach(option => {
-    if (option.classList.contains("isSelected")) {
-      let content = option.querySelector("content").innerHTML;
-      temp.push(content);
-    }
-  });
-  radios.forEach(radio => {
-    let selectedRadio = radio.querySelector(".isChecked");
-    if (selectedRadio != null) {
-      let content = selectedRadio.dataset.value;
-      temp.push(content);
-    }
-  });
-  data.push(temp);
-  chrome.storage.local.set({ responses: data }, function() {
-    console.log("posted value to storage");
+    let temp = [];
+    options.forEach(option => {
+      if (option.classList.contains("isSelected")) {
+        let content = option.querySelector("content").innerHTML;
+        temp.push(content);
+      }
+    });
+    radios.forEach(radio => {
+      let selectedRadio = radio.querySelector(".isChecked");
+      if (selectedRadio != null) {
+        let content = selectedRadio.dataset.value;
+        temp.push(content);
+      }
+    });
+    data.push(temp);
+    chrome.storage.local.set({ responses: data }, function() {
+      console.log("posted value to storage");
+    });
   });
 }
 
 submitButton.addEventListener("click", () => {
   getFormData();
 });
-submitButton2.addEventListener("click", () => {
-  getFormData();
-});
+// submitButton2.addEventListener("click", () => {
+//   getFormData();
+// });
 submitButton3.addEventListener("click", () => {
   getFormData();
 });
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let text = document.querySelector("p");
 let data = [];
-let table = document.querySelector("body");
-chrome.runtime.onUpdated.addListener(() => {
+let body = document.querySelector("body");
+body.onload = () => {
   chrome.storage.local.get(["responses"], function(result) {
-    result.responses.forEach((response) => {
-      let p = document.createElement("P");
-      p.innerHTML = response;
-      table.appendChild(p);
-      console.log(p);
-      console.log(table);
-    });
+    if (result.responses != undefined) {
+      result.responses.forEach(response => {
+        let p = document.createElement("P");
+        p.innerHTML = response;
+        body.appendChild(p);
+        console.log(p);
+        console.log(body);
+      });
+    }
   });
-});
+};
