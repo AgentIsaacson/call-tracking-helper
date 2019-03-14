ecdf147b7a86408313f13b95f4bec87bd8df80d9
AgentIsaacson <agentisaacson@gmail.com>
2019-03-14 13:58:50 -0600

    added # of calls tracked and posts to local storage

--- ring-central.js
+++ ring-central.js
@@ -1,34 +1,39 @@
 const name = document.querySelector(".titleText").innerText;
 let shouldTrackCall = false;
-const status = "Unavailable:";
-let tempStatus1;
-let tempStatus2;
+const callEndStatus = "Meeting";
+let tempStatus1 = "";
+let tempStatus2 = "";
 let callsTracked;
 chrome.storage.local.get(["callsTracked"], function(result) {
-  callsTracked = result.responses ? result.responses : 0;
+  callsTracked = result.callsTracked != undefined ? result.callsTracked : 0;
 });
 setInterval(() => {
   let people = document.querySelectorAll(".slick-row");
   let personData = "";
-  people.forEach((person) => {
+  people.forEach(person => {
     personData = person.innerText.split(/\n/);
-    tempStatus1 = personData[1];
-    console.log(tempStatus1 + "        " + tempStatus2);
-    if (
-      tempStatus1 !== tempStatus2 &&
-      personData[0].includes(name) &&
-      personData[1].includes(status)
-      && shouldTrackCall
-    ) {
-      callsTracked++;
-      console.log("call tracked:  " + callsTracked);
-      shouldTrackCall = false;
-    } else if (!personData[1].includes(status)) {
-      shouldTrackCall = true;
+    if (!personData[1].includes("(0")) {
+      tempStatus1 = personData[1];
+      if (personData[0].includes(name)) {
+        if (
+          tempStatus1 !== tempStatus2 &&
+          personData[1].includes(callEndStatus) &&
+          shouldTrackCall
+        ) {
+          callsTracked++;
+          shouldTrackCall = false;
+        } else if (!personData[1].includes(callEndStatus)) {
+          shouldTrackCall = true;
+        }
+        tempStatus2 = tempStatus1;
+      }
     }
-    tempStatus2 = tempStatus1;
   });
-  chrome.storage.local.set({ callsTracked: callsTracked }, function() {
-    console.log(callsTracked);
+  chrome.storage.local.get(["callsTracked"], function(result) {
+    console.log("localstorage: " + result.callsTracked);
+    console.log("To Push" + callsTracked);
+    chrome.storage.local.set({ callsTracked: callsTracked }, function() {
+      console.log(callsTracked);
+    });
   });
 }, 1000);
