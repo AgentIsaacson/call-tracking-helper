735a4db2d58c7841ae139d7a76568fc1ad6c169c
Araliatrix <agentisaacson@gmail.com>
2019-03-14 01:13:24 -0600

    added function to track status switch

--- ring-central.js
+++ ring-central.js
@@ -1,10 +1,23 @@
-const statuses = document.querySelectorAll(".grid-canvas")[1]
-statuses.addEventListener("change", () => {
-    console.log("changed")
-})
-
-console.log("working...")
-statuses.forEach(person => {
-    console.log("sup")
-})
-
+const name = document.querySelector(".titleText").innerText;
+let callsTracked = 0;
+let shouldTrackCall = false;
+const status = "Wrap Up";
+let tempStatus1;
+let tempStatus2;
+setInterval(() => {
+  let people = document.querySelectorAll(".slick-row");
+  let personData = "";
+  people.forEach((person) => {
+      personData = person.innerText.split(/\n/);
+      tempStatus1 = personData[1];
+      console.log(tempStatus1 + "        " + tempStatus2);
+    if (tempStatus1!==tempStatus2 && personData[0].includes(name) && personData[1].includes(status)) {
+      callsTracked++;
+      console.log("call tracked:  " + callsTracked);
+      shouldTrackCall = false;
+    } else if (!personData[1].includes(status)) {
+      shouldTrackCall = true;
+    }
+    tempStatus2 = tempStatus1;
+  });
+}, 5000);
