d89c2c7e59b04b94937cdcaa974897a6385ded79
Araliatrix <agentisaacson@gmail.com>
2019-03-19 22:18:16 -0600

    added catch for immediate jump to available

--- ring-central.js
+++ ring-central.js
@@ -11,24 +11,26 @@ setTimeout(() => {
     });
     let people = document.querySelectorAll(".slick-row");
     let personData = "";
-    people.forEach(person => {
+    people.forEach((person) => {
       personData = person.innerText.split(/\n/);
       if (personData.length > 1 && !personData[1].includes("(0")) {
         tempStatus1 = personData[1];
         if (personData[0].includes(name)) {
-            if (
-              tempStatus1 !== tempStatus2 &&
-              personData[1].includes(callEndStatus) && // add 'or Inbound straight to active' here
-              shouldTrackCall
-            ) {
-              callsTracked++;
-              shouldTrackCall = false;
-            } else if (!personData[1].includes(callEndStatus)) {
-              shouldTrackCall = true;
-            }
-            tempStatus2 = tempStatus1;
+          if (
+            tempStatus1 !== tempStatus2 &&
+            (personData[1].includes(callEndStatus) ||
+              (tempStatus1.includes("Inbound") &&
+                tempStatus2.includes("Available"))) &&
+            shouldTrackCall
+          ) {
+            callsTracked++;
+            shouldTrackCall = false;
+          } else if (!personData[1].includes(callEndStatus)) {
+            shouldTrackCall = true;
           }
+          tempStatus2 = tempStatus1;
         }
+      }
     });
     chrome.storage.local.get(["callsTracked"], function(result) {
       if (result.callsTracked === -1) {
